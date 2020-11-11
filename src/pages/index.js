26b2a7aac0c499ccdc727530cea888fe2232c41d
Tavis Lochhead <66540758+t-lochhead@users.noreply.github.com>
2020-11-10 19:57:25 -0500

    x

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,170 +12,161 @@ const whitish = `#deeaf3`
 
 export default () => {
   return (
-    <>
-      <Helmet title="tavis" />
+    <div
+      css={{
+        minHeight: `100vh`,
+        // borderTop: `${rhythm(3 / 4)} solid ${whitish}`,
+        // "@media (min-width: 420px)": {
+        border: `${rhythm(3 / 4)} solid ${whitish}`,
+        // },
+        background: `#51b3bf`,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Helmet defaultTitle="tavis" titleTemplate="tavis" />
       <div
         css={{
-          display: `flex`,
-          height: `100vh`,
-          background: whitish,
+          // minHeight: rhythm(1),
+          height: "60px",
+          display: "flex",
+          justifyContent: "space-between",
         }}
       >
-        {/* <div
-        css={{
-          position: `absolute`,
-          left: `33.333%`,
-          width: rhythm(1),
-          height: `22vh`,
-          zIndex: 10,
-          background: whitish,
-          borderBottomRightRadius: `34px 41%`,
-        }}
-      />
-      <div
-        css={{
-          position: `absolute`,
-          left: `33.33%`,
-          bottom: 0,
-          width: rhythm(1),
-          height: `24vh`,
-          zIndex: 10,
-          background: whitish,
-          borderTopLeftRadius: `34px 41%`,
-        }}
-      /> */}
         <div
           css={{
-            display: `flex`,
-            position: `absolute`,
-            margin: rhythm(3 / 4),
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            justifyContent: `center`,
-            alignItems: `center`,
-            background: `#51b3bf`,
-            flexWrap: "wrap",
-          }}
-        >
-          <div
-            css={{
-              display: `flex`,
-              flex: 1,
-              "@media (max-width: 420px)": {
-                flex: "unset",
-                alignItems: "flex-end",
-                height: "40vh",
-              },
-              justifyContent: "center",
-            }}
-          >
-            <h1
-              css={{
-                // width: `80%`,
-                fontSize: scale(6 / 5).fontSize,
-                lineHeight: 1.1,
-                margin: "0",
-                "@media (min-width: 800px)": {
-                  fontSize: scale(8 / 5).fontSize,
-                },
-                "@media (min-width: 1050px)": {
-                  fontSize: scale(12 / 5).fontSize,
-                },
-              }}
-            >
-              <span
-                style={{
-                  fontWeight: 200,
-                  color: "#f994ff",
-                  textShadow: "2px 2px #ffab94, 4px 4px #ffff94",
-                }}
-              >
-                Marketing
-              </span>
-              <br />
-              <span
-                style={{
-                  fontWeight: 500,
-                  color: "#ffab94",
-                  textShadow: "2px 2px #ffff94, 4px 4px #f994ff",
-                }}
-              >
-                Technology
-              </span>
-              <br />
-              <span
-                style={{
-                  fontWeight: 800,
-                  color: "#ffff94",
-                  textShadow: "2px 2px #f994ff, 4px 4px #ffab94",
-                }}
-              >
-                Data
-              </span>
-            </h1>
-          </div>
-          <div
-            css={{
-              display: `flex`,
-              flex: 1,
-              "@media (max-width: 420px)": {
-                flex: "unset",
-              },
-              justifyContent: "center",
-            }}
-          >
-            <img
-              alt="homegif"
-              src={homeGif}
-              style={{
-                objectFit: "contain",
-                width: "100%",
-                height: "50%",
-                maxHeight: "500px",
-                margin: "0",
-              }}
-            />
-          </div>
-        </div>
-        <div
-          css={{
+            float: `left`,
             color: whitish,
-            position: `absolute`,
-            top: rhythm(1.5),
-            zIndex: 10,
-            // transform: `rotate(90deg)`,
-            transformOrigin: `left top 0`,
-            // left: rhythm(2.5),
-            left: rhythm(1.5),
+            marginTop: rhythm(3 / 4),
+            marginLeft: rhythm(3 / 4),
             fontSize: scale(2 / 5).fontSize,
             lineHeight: scale(2 / 5).lineHeight,
-            "@media (min-width: 420px)": {
-              left: rhythm(1.5),
-              transform: `none`,
-            },
           }}
         >
           <NoStyleLink to="/">tavis</NoStyleLink>
         </div>
         <div
           css={{
+            float: `right`,
             color: whitish,
-            position: `absolute`,
-            right: rhythm(1.5),
-            top: rhythm(1.5),
-            zIndex: 10,
+            marginTop: rhythm(3 / 4),
+            marginRight: rhythm(3 / 4),
             fontSize: scale(2 / 5).fontSize,
             lineHeight: scale(2 / 5).lineHeight,
           }}
         >
-          {/* <NoStyleLink to="/blog/">portfolio</NoStyleLink>{" "} */}
           <NoStyleLink to="/blog/">blog</NoStyleLink>&nbsp;&nbsp;
           <NoStyleLink to="/about/">about</NoStyleLink>
-          {/* <NoStyleLink to="/about/">contact</NoStyleLink> */}
         </div>
       </div>
-    </>
+      <div
+        css={{
+          margin: `0 auto`,
+          display: `flex`,
+          flexDirection: "row",
+          alignItems: `center`,
+          flex: "1",
+          "@media (max-width: 420px)": {
+            flexDirection: "column",
+          },
+        }}
+      >
+        <div
+          css={{
+            display: `flex`,
+            flex: 1,
+            justifyContent: "center",
+            alignItems: `center`,
+          }}
+        >
+          <h1
+            css={{
+              // width: `80%`,
+              fontSize: scale(6 / 5).fontSize,
+              lineHeight: 1.1,
+              margin: "0",
+              "@media (min-width: 800px)": {
+                fontSize: scale(8 / 5).fontSize,
+              },
+              "@media (min-width: 1050px)": {
+                fontSize: scale(12 / 5).fontSize,
+              },
+            }}
+          >
+            <span
+              style={{
+                fontWeight: 200,
+                color: "#f994ff",
+                textShadow: "2px 2px #ffab94, 4px 4px #ffff94",
+              }}
+            >
+              Marketing
+            </span>
+            <br />
+            <span
+              style={{
+                fontWeight: 500,
+                color: "#ffab94",
+                textShadow: "2px 2px #ffff94, 4px 4px #f994ff",
+              }}
+            >
+              Technology
+            </span>
+            <br />
+            <span
+              style={{
+                fontWeight: 800,
+                color: "#ffff94",
+                textShadow: "2px 2px #f994ff, 4px 4px #ffab94",
+              }}
+            >
+              Data
+            </span>
+          </h1>
+        </div>
+        <div
+          css={{
+            display: `flex`,
+            flex: 1,
+            alignItems: `center`,
+            justifyContent: "center",
+          }}
+        >
+          <img
+            alt="homegif"
+            src={homeGif}
+            style={{
+              objectFit: "contain",
+              width: "80%",
+              height: "100%",
+              maxHeight: "500px",
+              margin: "0",
+            }}
+          />
+        </div>
+      </div>
+      <div
+        css={{
+          // minHeight: rhythm(1),
+          height: "60px",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          css={{
+            justifyContent: "center",
+            color: whitish,
+            marginBottom: rhythm(3 / 4),
+            fontSize: scale(2 / 5).fontSize,
+            lineHeight: scale(2 / 5).lineHeight,
+            display: "flex",
+            flex: "1",
+          }}
+        >
+          portfolio ->
+        </div>
+      </div>
+    </div>
   )
 }
