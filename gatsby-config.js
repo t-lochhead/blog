acaf57d6d219ccc4ad9d6b7fff7e47c3b1c9181f
Tavis Lochhead <66540758+t-lochhead@users.noreply.github.com>
2020-12-14 22:01:48 -0500

    x

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
             resolve: "gatsby-remark-external-links",
             options: {
               target: "_blank",
-              rel: "nofollow",
+              rel: "nofollow noreferrer noopener",
             },
           },
         ],
